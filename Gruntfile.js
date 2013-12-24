5bc9ea45338b01b7056bad5c4cff8ba0460fd5f3
Will Moritz <will@crowdfavorite.com>
2013-12-24 15:39:38 -0600

    added uglify and concatenation

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,26 +6,36 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        // Concatenate JS files
         concat: {
-            // 2. Configuration for concatenating files goes here.
             dist: {
                 src: [
-                    '/js/angular.min.js',
-                    '/js/app.js',
-                    '/js/factories.js',
-                    '/js/controllers.js',
-                    '/js/directives.js'
+                    'js/angular.min.js',
+                    'js/app.js',
+                    'js/factories.js',
+                    'js/controllers.js',
+                    'js/directives.js'
                 ],
                 dest: 'js/build/prod-common.js'
             }
+        },
+
+        // Minify JS files
+        uglify: {
+            build: {
+                src: 'js/build/prod-common.js',
+                dest: 'js/build/prod-common.min.js'
+            }
         }
+
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 
 };
