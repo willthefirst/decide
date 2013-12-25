076823035e032faad2d3fb89ffe283515a022712
Will Moritz <will@crowdfavorite.com>
2013-12-25 12:45:02 -0600

    taking a break from testing setup

--- tests/unit/controllerSpec.js
+++ tests/unit/controllerSpec.js
@@ -1,21 +1,94 @@
 'use strict';
 
-describe('Options', function() {
-    //initialise module
-    beforeEach(module('sensei.controllers'));
 
-    //params initialised in scope for tests
-    var ctrl, scope;
+describe('my services', function () {
+    beforeEach(module('sb.services'));
 
-    beforeEach(inject(function($controller) {
-        //get controller from $controller provider
-        scope = {};
-        ctrl = $controller('IndexController', {
-            $scope: scope
+    describe('version', function () {
+        it('should return current version', inject(function(version) {
+            expect(version).toEqual('0.0.1');
+        }));
+    });
+});
+
+describe('Options controller', function() {
+    var $compile;
+    var $rootScope;
+    var storage = {
+      getAllLocalInfo: function (){
+        return {
+           entries : [
+               {
+                   domain: "facebook.com",
+                   periodBeingUsed: false,
+                   periodLength: 1,
+                   periods: 4,
+                   periodsLeft: 4
+               },
+               {
+                   domain: "gmail.com",
+                   periodBeingUsed: false,
+                   periodLength: 1,
+                   periods: 5,
+                   periodsLeft: 2
+               },
+               {
+                   domain: "twitter.com",
+                   periodBeingUsed: false,
+                   periodLength: 1,
+                   periods: 4,
+                   periodsLeft: 1
+               },
+           ]
+        }
+      }
+    }
+
+    beforeEach(module('sensei'));
+
+    beforeEach(inject(function($rootScope, $controller) {
+        this.scope = $rootScope.$new();
+        $controller('Options', {
+          $scope: this.scope,
+          storage: storage
         });
     }));
 
-    it ('should add name parameter to scope', function() {
-        expect(scope.name).toBeDefined();
-    });
+    it('should return current version', inject(function(version) {
+        expect(version).toEqual('0.1');
+      }));
+
+   // var $scope = null;
+   //   var ctrl = null;
+
+   //   /* A mocked version of our service, someService
+   //    * we're mocking this so we have total control and we're
+   //    * testing this in isolation from any calls it might
+   //    * be making.
+   //    */
+
+
+
+   //  /* IMPORTANT!
+   //     * this is where we're setting up the $scope and
+   //     * calling the controller function on it, injecting
+   //     * all the important bits, like our mockService */
+   //    beforeEach(inject(function($rootScope, $controller) {
+   //      //create a scope object for us to use.
+   //      $scope = $rootScope.$new();
+
+   //      //now run that scope through the controller function,
+   //      //injecting any services or other injectables we need.
+   //      ctrl = $controller('MainCtrl', {
+   //        $scope: $scope,
+   //        someService: storage
+   //      });
+   //    }));
+
+   //    it('should start with foo and bar populated', function() {
+
+   //      //just assert. $scope was set up in beforeEach() (above)
+   //      expect($scope.entries[0].domain).toEqual('faceghbook.com');
+   //    });
+
 });
